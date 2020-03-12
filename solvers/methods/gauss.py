7363f5d32797bd972de8ccfaceb3e8df47f3579f
Matei Graura <grauramatei@gmail.com>
2020-03-13 00:10:38 +0200

    renamed reserved work & indentation

--- solvers/methods/gauss.py
+++ solvers/methods/gauss.py
@@ -1,18 +1,19 @@
-from numpy import array, zeros, fabs, linalg
+from numpy import zeros, fabs
 from copy import deepcopy
 
+
 def gaussian_elimination(matrix, coeff):
-	a = deepcopy(matrix)
-	b = deepcopy(coeff)
+    a = deepcopy(matrix)
+    b = deepcopy(coeff)
+    n = len(b)
     x = zeros(n, float)
-	n = len(b)
     eps = 0.5e-15
 
     # Elimination
     for k in range(n):
         if fabs(a[k, k]) < eps:
             for i in range(k + 1, n):
-                if (fabs(a[i, k] > a[k, k])):
+                if fabs(a[i, k] > a[k, k]):
                     a[[k, i]] = a[[i, k]]
                     b[[k, i]] = b[[i, k]]
                     break
@@ -29,11 +30,12 @@ def gaussian_elimination(matrix, coeff):
     x[n - 1] = b[n - 1] / a[n - 1][n - 1]
 
     for i in range(n - 2, -1, -1):
-        sum = 0
+        _sum = 0
         for j in range(i + 1, n):
-            sum += (x[j] * a[i, j])
-        x[i] = (b[i] - sum) / a[i, i]
+            _sum += (x[j] * a[i, j])
+        x[i] = (b[i] - _sum) / a[i, i]
 
-    return x
+    return x.reshape((n, 1))
+
 
 
